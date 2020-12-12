218fd3c27c5a77a1cde89118ca1c1fc0965a2f1b
Sushmitha Karanth <37877319+sushk15@users.noreply.github.com>
2020-12-12 23:13:47 +0530

    Create main.c

---
+++ postfix_exp/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "stack.h"
+
+int main()
+{
+
+    int i;
+    char postfix[POSTFIXSIZE];
+    printf("Four operators(*, /, +, -) in an expression and operand is single digit only.\n");   #Input example -> 6523+8*+3+*
+    printf(" \nEnter postfix expression,\npress right parenthesis ')' for end expression : ");
+
+    for (i = 0; i <= POSTFIXSIZE - 1; i++) {
+        scanf("%c", &postfix[i]);
+
+        if (postfix[i] == ')')
+        {
+            break;
+        } 
+    }
+
+    EvalPostfix(postfix);
+
+    return 0;
+}
